37ca7740ec366f48858ff026c0f34d3972d23e10
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2014-11-10 15:56:00 -0800

    cleanup

--- src/main/java/org/springframework/social/slideshare/api/impl/xml/GetSlideshowResponseMixin.java
+++ src/main/java/org/springframework/social/slideshare/api/impl/xml/GetSlideshowResponseMixin.java
@@ -3,30 +3,22 @@ package org.springframework.social.slideshare.api.impl.xml;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import org.springframework.social.slideshare.api.domain.Slideshow;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Tadaya Tsuyukubo
  */
 @JsonIgnoreProperties (ignoreUnknown = true)
-//@JacksonXmlRootElement(localName = "Tag")
 public abstract class GetSlideshowResponseMixin {
 
-	@JacksonXmlProperty ( localName = "Name" )
+	@JacksonXmlProperty (localName = "Name")
 	String name;
-	@JacksonXmlProperty ( localName = "Count" )
+	@JacksonXmlProperty (localName = "Count")
 	int count;
-	@JacksonXmlProperty ( localName = "Slideshow" )
-	@JacksonXmlElementWrapper(useWrapping = false)
+	@JacksonXmlProperty (localName = "Slideshow")
+	@JacksonXmlElementWrapper (useWrapping = false)
 	List<Slideshow> slideshows;
-//	Slideshow[] slideshows;
-
-//	@JacksonXmlProperty ( localName = "Slideshow" )
-////	@JacksonXmlElementWrapper(localName = "Slideshow")
-//	List<Slideshow> slideshows;
 
 }
